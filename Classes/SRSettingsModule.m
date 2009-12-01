2f6e2fe92bdc0012696dd55d53b138f0cd825123
Jan-Willem Buurlage <willem@moteoflife.net>
2009-12-01 20:25:23 +0100

    helderheid voorlopig met knoppen

--- Classes/SRSettingsModule.m
+++ Classes/SRSettingsModule.m
@@ -15,17 +15,22 @@
 		elements = [[NSMutableArray alloc] init];
 		
 		//laad elements in - sla op in textures
-		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(75,-45, 64,32) 
-															   texture:[[Texture2D alloc] initWithString:@"HELDERHEID" dimensions:CGSizeMake(64,32) alignment:UITextAlignmentLeft fontName:@"Helvetica-Bold" fontSize:9] 
-															identifier:@"text-transparent"
-															 clickable:NO]];
-
-		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(200, -44, 32, 32)
+		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(70,-45, 32,32) 
+															   texture:[[Texture2D alloc] initWithImage:[UIImage imageNamed:@"brightness_minus.png"]] 
+															identifier:@"brightness_minus" 
+															 clickable:YES]];		
+		
+		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(110,-45, 32,32) 
+															   texture:[[Texture2D alloc] initWithImage:[UIImage imageNamed:@"brightness_plus.png"]] 
+															identifier:@"brightness_plus" 
+															 clickable:YES]];		
+		
+		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(150, -44, 32, 32)
 															   texture:[[Texture2D alloc] initWithImage:[UIImage imageNamed:@"red.png"]] 
 															identifier:@"red" 
 															 clickable:YES]];		
 		//laad elements in - sla op in textures
-		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(240, -44, 32, 32)
+		[elements addObject:[[SRInterfaceElement alloc] initWithBounds:CGRectMake(190, -44, 32, 32)
 															   texture:[[Texture2D alloc] initWithImage:[UIImage imageNamed:@"constellation.png"]] 
 															identifier:@"constellation" 
 															 clickable:YES]];
