b599600e3c5d9fab2d28a86f73c6c1a003290bcc
Jan-Willem Buurlage <willem@moteoflife.net>
2009-12-02 20:06:12 +0100

    Classes/

---
+++ Classes/SRConstellationLine.h
@@ -0,0 +1,16 @@
+//
+//  SRConstellationLine.h
+//  Sterren
+//
+//  Created by Jan-Willem Buurlage on 02-12-09.
+//  Copyright 2009 Web6.nl Diensten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SRConstellationLine : NSObject {
+
+}
+
+@end
---
+++ Classes/SRConstellationLine.m
@@ -0,0 +1,14 @@
+//
+//  SRConstellationLine.m
+//  Sterren
+//
+//  Created by Jan-Willem Buurlage on 02-12-09.
+//  Copyright 2009 Web6.nl Diensten. All rights reserved.
+//
+
+#import "SRConstellationLine.h"
+
+
+@implementation SRConstellationLine
+
+@end
