739ddd1dd2401bed69e027b8573c8db6140811f3
Thijs Scheepers <thijs@moteoflife.net>
2009-12-23 13:08:34 +0100

    Aanpassing aan het bereken van geroteerde locatie m-objecten

--- Classes/SRMessier.m
+++ Classes/SRMessier.m
@@ -18,6 +18,41 @@
 	float y = position.y;
 	float z = position.z;
 	
+	id appDelegate = [[UIApplication sharedApplication] delegate];
+	
+	float rotationY = (90-[[appDelegate location] latitude])*(M_PI/180);
+	float rotationZ1 = [[appDelegate location] longitude]*(M_PI/180);
+	float rotationZ2 = [[appDelegate timeManager] elapsed]*(M_PI/180);
+	
+	float maX,maY,maZ;
+	
+	// Matrix vermenigvuldiging met draai om de  z-as (tijd)
+	maX = (cos(rotationZ2)*brX+(-sin(rotationZ2)*brY)+0*brZ);
+	maY = (sin(rotationZ2)*brX+cos(rotationZ2)*brY+0*brZ);
+	maZ = (0*brX+0*brY+1*brZ);
+	
+	brX = maX;
+	brY = maY;
+	brZ = maZ;
+	
+	// Matrix vermenigvuldiging met draai om de  z-as (locatie)
+	maX = (cos(rotationZ1)*brX+(-sin(rotationZ1)*brY)+0*brZ);
+	maY = (sin(rotationZ1)*brX+cos(rotationZ1)*brY+0*brZ);
+	maZ = (0*brX+0*brY+1*brZ);
+	
+	brX = maX;
+	brY = maY;
+	brZ = maZ;
+	
+	// Matrix vermenigvuldiging met draai om de y-as (locatie)
+	maX = (cos(rotationY)*brX+0*brY+sin(rotationY)*brZ);
+	maY = (0*brX+1*brY+0*brZ);
+	maZ = ((-sin(rotationY)*brX)+0*brY+cos(rotationY)*brZ);
+	
+	brX = maX;
+	brY = maY;
+	brZ = maZ;
+	
 	Vertex3D result;
 	return result;
 }
