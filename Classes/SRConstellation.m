bfe2d3b2a9bbf0fdbfac66cecaf3df12ac84dda3
Jan-Willem Buurlage <willem@moteoflife.net>
2009-12-25 11:50:43 +0100

    latijnse namen sterrenbeelden ingevoerd, spaties geïmplementeerd

--- Classes/SRConstellation.m
+++ Classes/SRConstellation.m
@@ -27,11 +27,7 @@
 	for(SRConstellationLine* aLine in lines) {
 		RATemp = atan2(aLine.start.y, aLine.start.x);
 		
-		if(i == 0) { if(RATemp > 0) { stayHigh = TRUE; } else { stayHigh = FALSE; } ++i; 		
-			if([name isEqualToString:@"UMa"] || [name isEqualToString:@"Peg"]) {
-				if(stayHigh) { NSLog(@"high");  } else { NSLog(@"low"); } 
-			}
-		}
+		if(i == 0) { if(RATemp > 0) { stayHigh = TRUE; } else { stayHigh = FALSE; } ++i; }
 		
 		
 		if(stayHigh && RATemp < M_PI) {
@@ -41,10 +37,6 @@
 			RATemp -= 2*M_PI;
 		}
 		totRA += RATemp;
-		
-		if([name isEqualToString:@"UMa"] || [name isEqualToString:@"Peg"]) {
-			NSLog(@"%f",RATemp * (12/M_PI));
-		}
 		
 		RATemp = atan2(aLine.end.y, aLine.end.x);
 		if(stayHigh && RATemp < M_PI) {
@@ -54,11 +46,7 @@
 			RATemp -= 2*M_PI;
 		}
 		totRA += RATemp;
-		
-		if([name isEqualToString:@"UMa"] || [name isEqualToString:@"Peg"]) {
-			NSLog(@"%f",RATemp * (12/M_PI));
-		}
-		
+				
 		totDec += acos((aLine.start.z)/sqrt(pow(aLine.start.x,2)+pow(aLine.start.y,2)+pow(aLine.start.z,2))) + acos((aLine.end.z)/sqrt(pow(aLine.end.x,2)+pow(aLine.end.y,2)+pow(aLine.end.z,2)));
 	}
 	
@@ -67,7 +55,7 @@
 	if(ra < 0) { ra += 360; }	
 	dec = 90 - ((180/M_PI) * (totDec/([lines count] * 2)));
 	
-	//NSLog(@"%@: .. RA: %f Dec: %f", name, ra, dec); 
+	NSLog(@"%@: .. RA: %f Dec: %f", name, ra, dec); 
 	
 	ra += 180;
 	
@@ -75,7 +63,7 @@
 								   20.0 * sin(totDec/([lines count] * 2)) * sin(totRA/([lines count] * 2)),
 								   20.0 * cos(totDec/([lines count] * 2)));
 	
-	nameTexture = [[Texture2D alloc] initWithString:name dimensions:CGSizeMake(64, 64) alignment:UITextAlignmentCenter fontName:@"Helvetica-Bold" fontSize:1.0];
+	nameTexture = [[Texture2D alloc] initWithString:[name stringByReplacingOccurrencesOfString:@"-" withString:@" "] dimensions:CGSizeMake(64, 64) alignment:UITextAlignmentCenter fontName:@"Helvetica-Bold" fontSize:1.0];
 }
 
 -(void)draw {
