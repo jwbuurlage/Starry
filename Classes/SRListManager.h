a465bc8f271fc9c8910341b55765edd35d439efd
Jan-Willem Buurlage <willem@moteoflife.net>
2010-01-06 19:44:56 +0100

    lijst class toegevoegd

---
+++ Classes/SRListManager.h
@@ -0,0 +1,16 @@
+//
+//  SRListManager.h
+//  Sterren
+//
+//  Created by Jan-Willem Buurlage on 06-01-10.
+//  Copyright 2010 Web6.nl Diensten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SRListManager : NSObject {
+
+}
+
+@end
---
+++ Classes/SRListManager.m
@@ -0,0 +1,14 @@
+//
+//  SRListManager.m
+//  Sterren
+//
+//  Created by Jan-Willem Buurlage on 06-01-10.
+//  Copyright 2010 Web6.nl Diensten. All rights reserved.
+//
+
+#import "SRListManager.h"
+
+
+@implementation SRListManager
+
+@end
