407a11171bec71268bffea42d7fa00ec39ee67be
Thijs Scheepers <thijs@Thijs-Scheeperss-MacBook-Pro.local>
2010-03-18 19:15:02 +0100

    Aanpassingen voor Apple, incl aanpassingen Maan en DatumFormat

--- Classes/SRMoon.m
+++ Classes/SRMoon.m
@@ -34,7 +34,7 @@
 	float second = [weekdayComponents second];
 	
 	//dagen sinds tabel geldig is http://www.astro.uu.nl/~strous/AA/en/reken/hemelpositie.html
-	float d = 367*year - (7*(year + ((month+9)/12)))/4 + (275*month)/9 + day - 730530 + ((hour / 24) + (minute / 1440) + (second / 86400));
+	float d = 367*year - (7*(year + ((month+9)/12)))/4 + (275*month)/9 + day - 730532 + ((hour / 24) + (minute / 1440) + (second / 86400));
 	//d = 1460.5;
 	
 	//positie uitrekenen:
